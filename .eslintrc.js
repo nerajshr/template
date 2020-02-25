99669a5f925c815a3445eeed281be68a58c8820a
ping <ping@pings-MacBook-Pro.local>
2020-02-25 14:36:27 +0530

    Third Commit for react-navigation v4

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,24 @@ module.exports = {
   },
   'rules': {
     'no-use-before-define': 'off',
-    'react/jsx-filename-extension': 'off',
     'react/prop-types': 'off',
-    'comma-dangle': 'off'
+    'comma-dangle': 'off',
+    "react/prefer-stateless-function": 0,
+    "max-len": 0,
+    "no-restricted-syntax": 0,
+    "import/first": 0,
+    "quotes": 0,
+    "react/jsx-filename-extension": 0,
+    "react/forbid-prop-types": 0,
+    "global-require": 0,
+    "react/no-array-index-key": 0,
+    "import/prefer-default-export": 0,
+    "no-underscore-dangle": 0,
+    "no-mixed-operators": 0,
+    "no-plusplus": 0,
+    "no-lonely-if": 0,
+    "react/sort-comp": 0,
+    "react/destructuring-assignment":0,
   },
   'globals': {
     "fetch": false
